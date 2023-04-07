bea243c0a636aae5a4ee751a730c72aa09588f02
Alex Wetzel <wetzela@live.com>
2023-04-06 23:46:44 -0400

    Added resume text and gh pages

--- portfolio/src/pages/Resume.js
+++ portfolio/src/pages/Resume.js
@@ -1,7 +1,84 @@
 function Resume() {
 
   return(
-    <p>Coming soon!</p>
+    <>
+      <h1>Writing Example: Resume and Cover Letter</h1>
+      <p>About...</p>
+      <br />
+
+      <h3>Alexander Wetzel</h3>
+      <p>wetzela@live.com | 407-913-8453 | Winter Garden, Florida</p>
+      <p>Linkedin | GitHub | Portfolio</p>
+      <br />
+
+      <p>Computer Science student seeking an internship to refine my skills and contribute as a team player working to create exceptional software. </p>
+      <br />
+
+      <h4>Technical Skills</h4>
+      <p>HTML / CSS – Intermediate</p>
+      <p>JavaScript – Intermediate</p>
+      <p>MySQL – Novice</p>
+      <p>Java - Beginner</p>
+      <p>React – Intermediate</p>
+      <p>Node – Intermediate</p>
+      <p>C – Novice</p>
+      <p>Github – Intermediate</p>
+      <p>MongoDB - Intermediate  </p>
+      <p>PHP - Beginner </p>
+      <br />
+      
+      <h4>Education</h4>
+      <p>Tunnelight (Senior Project) 2022-2023</p>
+      <p>Backend Developer</p>
+      <ul>
+        <li>Web application connecting homeless people to resources.</li>
+        <li>Collaborated with 4 other students and sponsor to design and develop application from scratch.</li>
+        <li>Contributed to design document consisting of over 150 pages.</li>
+        <li>Held weekly meetings reviewing progress and planning tasks. </li>
+      </ul>
+      <br />
+      
+      <p>Sandwich Store (Personal Project) 2018-2019</p>
+      <p>Full Stack Developer</p>
+      <ul>
+        <li>A mock point-of-sale kiosk app where users can order food.</li>
+        <li>Designed system to track inventory as food is prepared.</li>
+      </ul>
+      <br />
+
+      <p>Monster Hunter Arms (Personal Project)	2018</p>
+      <p>Full Stack Developer</p>
+      <ul>
+        <li>Weapon tutorial site and interactive flowchart for the video game Monster Hunter World</li>
+        <li>Implemented gamepad API to simulate game inputs.</li>
+      </ul>
+      <br />
+      
+      <h4>Previous Experience</h4>
+      <p>Barback (Orlando, FL)	2021-present </p>
+      <p>The Boathouse Disney Springs</p>
+      <ul>
+        <li>Maintain well-stocked bars for bartenders to provide efficient service.</li>
+        <li>Communicated with guests to answer questions and ensure a quality experience. </li>
+      </ul>
+      <br />
+
+      <p>Food and Beverage Steward (Orlando, FL)	2017 </p>
+      <p>Walt Disney World</p>
+      <ul>
+        <li>Adapt to different roles in the cleaning process to clean dishes efficiently.</li>
+        <li>Remain informed on health and safety regulations.</li>
+      </ul>
+      <br />
+
+      <p>Busser/Food Runner (Huntersville, NC)	2015-2016</p>
+      <p>Smoke Modern Barbeque</p>
+      <ul>
+        <li>Remain knowledgeable of menu options to optimize customer experience.</li>
+        <li>Coordinated with front and back of house to work efficiently.</li>
+      </ul>
+      <br />
+    </>
   )
 }
 
